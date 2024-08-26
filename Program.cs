cf79e777e63297ed8da717d06e7c8feb768c44da
Mariano Oliva <nicolas.oliva.montivero@gmail.com>
2024-08-26 20:56:28 -0300

    ejercicio2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el primer número entero:");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número entero:");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        bool esParNumero1 = numero1 % 2 == 0;
+        bool esParNumero2 = numero2 % 2 == 0;
+
+        if (esParNumero1 != esParNumero2)
+        {
+            Console.WriteLine("Uno de los números es par.");
+        }
+        else
+        {
+            Console.WriteLine("Ambos números son pares o ambos son impares.");
+        }
+    }
+}
